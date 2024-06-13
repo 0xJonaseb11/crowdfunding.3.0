10e8e710cd07afdb2d6a5d4bfc4455b96c000231
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-13 08:08:10 +0200

    update campaignform

--- client/src/components/CampaignForm.jsx
+++ client/src/components/CampaignForm.jsx
@@ -21,6 +21,59 @@ const CampaignForm = ({contract, provider}) => {
             image
         );
 
-        
-    }
+        await tx.wait();
+        alert("Campaign created successfully");
+    };
+
+    return (
+        <div>
+            <Typography variant="h6">Create Campaign</Typography>
+            <TextField
+            label="Title"
+            value={title}
+            onChange={(e) =>setTitle(e.target.value)}
+            fullWidth
+            margin="normal"/>
+
+            <TextField
+            label="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            fullWidth
+            margin="normal"
+            />
+
+            <TextField
+            label="Target (ETH)"
+            value={target}
+            onChange={(e) => setTarget(e.target.value)}
+            fullWidth
+            margin="normal"
+            />
+
+            < TextField
+            label="deadline"
+            type="date"
+            value={deadline}
+            onChange={(e) => setDeadline(e.target.value)}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{shrink: true}}
+            />
+
+            < TextField
+            label="Image URL"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            fullWidth
+            margin="normal"
+            />
+
+            <Button variant="contained" color="primary" onclick={createCampaign}>
+                create
+            </Button>
+        </div>
+    )
 };
+
+export default CampaignForm
